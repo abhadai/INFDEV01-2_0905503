aee3f4116939028e3d6596173e2cbd35c02a018f
Andy Bhadai <abhadai@gmail.com>
2015-12-09 19:22:20 +0100

    Added a bit of simulation logic

--- DEV 2 Game template (py 3_4)/DEV 2 Game template/Game.py
+++ DEV 2 Game template (py 3_4)/DEV 2 Game template/Game.py
@@ -17,25 +17,35 @@ entry_tile = build_square_matrix(board_size, offset)
 
 # Create cars
 car_list = Empty
-car_list = Node(Car(10, 10), car_list)
+car_list = Node(Car(entry_tile.Position.X, entry_tile.Position.Y), car_list)
 
-def Update(car_list):
+def Update(car_list, count):
+ 
+  if count % 5 == 0:
+      car_list = Node(Car(entry_tile.Position.X, entry_tile.Position.Y), car_list)
 
-#TODO: add the logic of your cars here
-#HINT For filtering reasons we return a list (of cars?)
+  # Make the cars move around    
+  if not (car_list.IsEmpty):
+      
+      if(entry_tile.Traverseable):
+          car_list.Value.PosX += entry_tile.Position.X + 1
+
+  #TODO: add the logic of your cars here
+  #HINT For filtering reasons we return a list (of cars?)
   return car_list
 
 
-def Draw(cars):
+def Draw(car_list):
 #TODO: add the draw of your cars below
 #
 # Use the following code to draw your car. 
 # HINT: POSITION_X and POSITION_Y represent the position of the car to draw on the screen
 #
-# _width = int(offset / 3)
-# screen.blit(pygame.transform.scale(car_texture, (_width, _width)), 
-#                  (_width + POSITION_X * offset, 
-#                   _width + POSITION_Y * offset))
+  _width = int(offset / 3)
+  screen.blit(pygame.transform.scale(car_texture, (_width, _width)), 
+                (_width + car_list.Value.PosX * offset, 
+                _width + car_list.Value.PosY * offset))
+  print(car_list.Value.PosX)
   print("<3")
 
 
@@ -43,13 +53,16 @@ def Draw(cars):
 def Main():
   start = time.time()
   cars = Empty
+  count = 0
+
   while True:
     pygame.event.wait()    
     screen.fill(green)  
     entry_tile.Reset()
     entry_tile.Draw(screen)
 
-    cars = Update(cars)
+    count += 1
+    cars = Update(car_list, count)
     Draw(cars)
 
     pygame.display.flip()
